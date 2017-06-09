d41db15e55c89f5d508bdba46517a4f9632385b2
LenaKuchko <kuchkoel@gmail.com>
2017-06-09 08:34:51 -0700

    add initial files and folders

---
+++ Tests/StylistTest.cs
@@ -0,0 +1,18 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using HairSalon.Objects;
+
+namespace HairSalon
+{
+  [Collection("HairSalon")]
+
+  public class StylistTest
+  {
+    public StylistTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
+    }
+  }
+}
